600a43ee775c4adda3e86c45796341b254228f1d
olatisunkanmi <igbasanolasunkanmi080@gmail.com>
2024-01-06 17:32:32 +0000

    feat: auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { UserSignUpDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,9 @@
+
+
+export class UserSignUpDto {
+    readonly firstName: string;
+    readonly lastName: string;
+    readonly email: string;
+    readonly password: string;
+    readonly role: string;
+}
